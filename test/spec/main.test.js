bf9cd37185beb0bd0af21c3f0b0985277dccb831
Luke Lancaster <luketlancaster@gmail.com>
2015-02-05 11:31:23 -0600

    multiple tests pass, add lodash and jquery

--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -12,3 +12,18 @@ describe('hello', function() {
     hello().should.equal('world');
   });
 });
+
+describe('getStock', function() {
+  it('should return a stock object', function(done) {
+    getStock('AAPL', function(stock) {
+      stock.Name.should.equal('Apple Inc');
+      done();
+    });
+  });
+  it('should return another stock object', function(done) {
+    getStock('AMZN', function(stock) {
+      stock.Name.should.equal('Amazon.com Inc');
+      done();
+    });
+  });
+});
